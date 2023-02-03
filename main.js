b6f80bfbe745fe1798669ea4489998373a298eb9
Ziyu Wang <68621123+wang-ziyu1@users.noreply.github.com>
2023-02-03 18:34:07 -0500

    slim down the dark mode

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function darkMode() {
-    var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "dark-mode";
-    content.innerText = "Dark Mode is ON";
-  }
-  function lightMode() {
-    var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "light-mode";
-    content.innerText = "Dark Mode is OFF";
-  }
+      function darkMode() {
+        var element = document.body;
+        element.className = "dark-mode";
+      }
+      function lightMode() {
+        var element = document.body;
+        element.className = "light-mode";
+      }
