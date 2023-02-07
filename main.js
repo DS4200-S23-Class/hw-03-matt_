03478a2e321f7719a39f8ced0c667a65f7594ed8
Matt Cerillo <mcerillo14@gmail.com>
2023-02-07 14:11:18 -0500

    Change

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 function darkMode() {
   var element = document.body;
-  element.className = "dark-mode";
+  element.classList.toggle("dark-mode");
  }
  
- function lightMode() {
-  var element = document.body;
-  element.className = "light-mode";
- }
